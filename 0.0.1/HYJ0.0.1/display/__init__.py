a623f4f26514e8d8f1f710850d058bc5c36dfd83
AAA_shuibiao <AAA_shuibiao@163.com>
2019-09-19 11:58:52 -0400

    Doing UI.psd
    some PHOTOSHOP progress

---
+++ 0.0.1/HYJ0.0.1/display/__init__.py
@@ -0,0 +1,4 @@
+if __name__ == "__main__": raise SystemError("Incorrect starting file")
+
+screen_size = (1920,1080)
+texture_pack = "standard"
---
+++ 0.0.1/HYJ0.0.1/display/menu/__init__.py
@@ -0,0 +1,1 @@
+if __name__ == "__main__": raise SystemError("Incorrect starting file")
