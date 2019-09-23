3ca8bdc13a8fcb643e78e5a748b10e08e679902e
AAA_shuibiao <AAA_shuibiao@163.com>
2019-09-23 17:31:06 -0400

    Done UI framework
    It is a big progress on start_interface.

--- 0.0.1/HYJ0.0.1/display/menu/UItools.py
+++ 0.0.1/HYJ0.0.1/display/menu/UItools.py
@@ -5,12 +5,14 @@ import pygame
 
 import display
 
+
 def loadpics(route, namelist):
     for name in namelist:
         display.picbuf[name.split(".")[0]] =\
             pygame.image.load(route + name).convert_alpha()
 
-def print_text(text, poz, color = pygame.Color(0,0,0), size = None):
+
+def print_text(text, poz = None, color = pygame.Color(0,0,0), size = None, surface = "Not_given"):
     if type(text) != str: text = str(text)
 
     FS = display.font.render(text, True, color)
@@ -20,4 +22,123 @@ def print_text(text, poz, color = pygame.Color(0,0,0), size = None):
 
     if size: FS = pygame.transform.scale(FS, size)
 
-    display.screen.blit(FS, poz)
+    if surface == "Not_given":
+        return FS
+    else: surface.blit(FS, poz)
+
+
+class Pic(object):
+    def __init__(self, name, surface, rect = None, poz = None):
+        self.name = str( name )
+        if rect: surface = pygame.transform.scale(surface, rect.size)
+        self.surface = surface
+        if not rect: rect = pygame.Rect(poz, surface.get_size())
+        self.rect = rect
+        self.UI = None
+
+    def update(self):
+        self.UI.screen.blit(self.surface, self.rect.topleft)
+
+
+class Button(object):
+    def __init__(self, name, text, rect, flag,\
+    text_color       = pygame.Color(0,0,0),\
+    background_color = pygame.Color(255,255,255),\
+    frame_color      = pygame.Color(255,0,0)):
+
+        self.name             = str( name )
+        self.rect             = pygame.Rect( rect )
+        self.text             = str( text )
+        self.flag             = flag
+        self.surface          = pygame.Surface( self.rect.size )
+        self.text_color       = self.color_fomatting( text_color )
+        self.background_color = self.color_fomatting( background_color )
+        self.frame_color      = self.color_fomatting( frame_color )
+        self.UI               = None
+
+        self.init_button_surface()
+
+    @staticmethod
+    def color_fomatting(color):
+        if type(color) == pygame.Color: return color
+        elif type(color) == tuple:
+            return pygame.Color(color)
+        else: raise ValueError
+
+    def init_button_surface(self):
+        surface = self.surface
+        surface.fill(self.background_color)
+        button_frame_rect = self.rect.copy()
+        button_frame_rect.topleft = (0,0)
+        pygame.draw.rect(surface, self.frame_color, button_frame_rect, 5)
+        print_text(self.text, (0,0), self.text_color,\
+            self.rect.size, surface)
+
+
+    def collide(self, poz):
+        if self.rect.collidepoint(poz):
+            self.UI.flag = self.flag
+
+
+    def update(self):
+        self.UI.screen.blit(self.surface, self.rect.topleft)
+
+
+class Page(object):
+    def __init__(self, name):
+        self.name = str( name )
+        self.pics = {}
+        self.buttons = {}
+        self.UI = None
+    
+    def add_pic(self, pic):
+        pic.UI = self.UI
+        self.pics[pic.name] = pic
+
+    def add_button(self, button):
+        button.UI = self.UI
+        self.buttons[button.name] = button
+
+    def remove_pic(self, name):
+        del self.pics[name]
+
+    def remove_button(self, name):
+        del self.buttons[name]
+
+    def collide(self, poz):
+        for buttonName in self.buttons:
+            button = self.buttons[buttonName]
+            button.collide(poz)
+
+    def update(self):
+        for picName in self.pics:
+            self.pics[picName].update()
+        for buttonName in self.buttons:
+            self.buttons[buttonName].update()
+
+
+class UI(object):
+    def __init__(self, name, screen):
+        self.name = str( name )
+        self.screen = screen
+        self.flag = None
+        self.pages = {}
+
+    def add_page(self, page):
+        page.UI = self
+        self.pages[page.name] = page
+        if self.flag == None: self.flag = page
+
+    def remove_page(self, name):
+        del self.pages[name]
+
+    def add_pages(self, pages):
+        for page in pages:
+            self.add_page(page)
+
+    def remove_pages(self, pages):
+        for page in pages:
+            self.remove_page(page)
+
+    def update(self):
+        self.flag.update()
--- 0.0.1/HYJ0.0.1/display/menu/start_interface.py
+++ 0.0.1/HYJ0.0.1/display/menu/start_interface.py
@@ -17,17 +17,33 @@ def loadpics():
 def main_loop():
     pygame.display.set_icon(display.picbuf["Icon"])
     screen = display.screen
-    flag = "press_any_key"
-    
+
+    UI = display.menu.UItools.UI("start_interface", display.screen)
+
+    press_any_key = display.menu.UItools.Page("press_any_key")
+
+    main_menu = display.menu.UItools.Page("main_menu")
+
+    UI.add_pages( [press_any_key, main_menu] )
+
+    press_any_key.add_pic(
+        display.menu.UItools.Pic(
+            "Unnamed", display.menu.UItools.print_text(
+                "press any key to start..", size = 0.5
+            ), poz = (800,1000)
+        )
+    )
+
+    main_menu.add_button(
+        display.menu.UItools.Button(
+            "back", "Back", pygame.Rect((500,500), (200,100)), press_any_key
+        )
+    )
+
     while True:
         loop_start_time = time.time()
 
         screen.blit(display.picbuf["BG"], (0,0))
-
-        if flag == "press_any_key":
-            display.menu.UItools.print_text(
-                "Press any key to start...", (800,1000), size = 0.3
-            )
 
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -39,12 +55,15 @@ def main_loop():
                     pygame.quit()
                     sys.exit()
                 else:
-                    if flag == "press_any_key":
-                        flag = "menu"
+                    UI.flag = main_menu
             
             if event.type == MOUSEBUTTONDOWN:
-                print(pygame.mouse.get_pos())
+                mouse_poz = pygame.mouse.get_pos()
+                UI.flag.collide(mouse_poz)
+                #print(mouse_poz) #temp
 
+        UI.update()
+        
         pygame.display.update()
 
         while time.time()-loop_start_time < (1/30): time.sleep(0.001)
