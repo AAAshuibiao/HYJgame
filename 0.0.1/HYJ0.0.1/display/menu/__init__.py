188c2e4d7020e9e558656dd12d62f7dc359b1f1d
AAA_shuibiao <AAA_shuibiao@163.com>
2019-10-31 14:34:35 -0400

    Commented some plans

--- 0.0.1/HYJ0.0.1/display/menu/__init__.py
+++ 0.0.1/HYJ0.0.1/display/menu/__init__.py
@@ -2,5 +2,10 @@ if __name__ == "__main__": raise SystemError("Incorrect starting file")
 
 import display
 
+# to do:
+#   change UI structure, combine "Pic" and "Button", REDO update logic, seperate display and update
+#   change "UI_Script" design, stucturelize it as like JSON or Java class define or something better
+#   or change the "UI_Script" design to 100% program_generate
+
 __all__ = ["UItools", "start_interface", "game_interface", "UI_script", "PLtools"]
 from display.menu import *
