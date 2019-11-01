f7061d5f6789d5c383f65f59591c1f2f2a385833
AAA_shuibiao <AAA_shuibiao@163.com>
2019-11-01 09:49:52 -0400

    TO DO updated

--- 0.0.1/HYJ0.0.1/display/menu/__init__.py
+++ 0.0.1/HYJ0.0.1/display/menu/__init__.py
@@ -6,6 +6,8 @@ import display
 #   change UI structure, combine "Pic" and "Button", REDO update logic, seperate display and update
 #   change "UI_Script" design, stucturelize it as like JSON or Java class define or something better
 #   or change the "UI_Script" design to 100% program_generate
+#   implicitly do draw_button_surface
+#   make a git_ignored, local saved setting system
 
 #bug_fix:
 #   text_color may not working
